bb7ab664b0046fe97615f3fbb71864d7b255c7bc
StudenxFinance <studenxfinance@gmail.com>
2024-06-26 20:42:47 +0300

    videos

--- src/Sections/Section6.jsx
+++ src/Sections/Section6.jsx
@@ -8,7 +8,11 @@ export const Section6 = () => {
         <section className="six">
             <div className="six-content">
                 <h1 className="six-text" >LET'S GET READY FOR SUMMER</h1>
-                <video src="/sodavideo.mp4" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
+                <div className="six-videos">
+                    <video src="/vid1.mov" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
+                    <video src="/vid2.mov" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
+                    <video src="/vid3.mov" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
+                </div>
             </div>
         </section>
     );
